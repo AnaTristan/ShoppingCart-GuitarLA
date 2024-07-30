11c695ec539329dcf8abe41a7e746d21e011ecc4
Ana Lucia Coronado Tristan <alct0397@gmail.com>
2024-07-29 20:18:25 -0600

    Agregando informacion de db por guitarra / Added db guitar info to component

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 
-const Guitar = () => {
+const Guitar = ({ guitar }) => {
+  const { id, name, image, description, price } = guitar;
+
+  const handleClick = () => {};
+
   return (
     <>
       <div className="col-md-6 col-lg-4 my-4 row align-items-center">
         <div className="col-4">
           <img
             className="img-fluid"
-            src="./img/guitarra_01.jpg"
+            src={`./img/${image}.jpg`}
+            // src="./img/guitarra_01.jpg"
             alt="imagen guitarra"
           />
         </div>
         <div className="col-8">
-          <h3 className="text-black fs-4 fw-bold text-uppercase">Lukather</h3>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae
-            labore odit magnam in autem nesciunt, amet deserunt
-          </p>
-          <p className="fw-black text-primary fs-3">$299</p>
-          <button type="button" className="btn btn-dark w-100">
+          <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+          <p>{description}</p>
+          <p className="fw-black text-primary fs-3">{price}</p>
+          <button
+            type="button"
+            className="btn btn-dark w-100"
+            onClick={() => {
+              handleClick(id);
+            }}
+          >
             Agregar al Carrito
           </button>
         </div>
