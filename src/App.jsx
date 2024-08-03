dd62646d7875a9e243513b096f4ab0d6f2493726
Ana Lucia Coronado Tristan <alct0397@gmail.com>
2024-08-02 20:43:35 -0600

    Agregando custom hook para carrito / added cart custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,20 @@
-import { useState, useEffect } from "react";
 import PageHeader from "./components/PageHeader";
 import PageFooter from "./components/pageFooter";
 import Guitar from "./components/Guitar";
-import { db } from "./data/db";
+import { useCart } from "./hooks/useCart";
 
 import "./index.css";
 
 function App() {
-  const initialCart = () => {
-    const localStorageCart = localStorage.getItem("cart");
-    return localStorageCart ? JSON.parse(localStorageCart) : [];
-  };
-  const [data] = useState(db);
-  const [cart, setCart] = useState(initialCart);
-
-  function addToCart(item) {
-    const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
-
-    if (itemExists < 0) {
-      item.quantity = 1;
-      setCart([...cart, item]);
-    } else {
-      const updateCart = [...cart];
-      updateCart[itemExists].quantity++;
-      setCart(updateCart);
-    }
-  }
-
-  function removeFromCart(id) {
-    setCart((prevCart) => prevCart.filter((guitar) => guitar.id !== id));
-  }
-
-  function increaseQuantity(id) {
-    const updateCart = cart.map((item) => {
-      if (item.id === id) {
-        return {
-          ...item,
-          quantity: item.quantity + 1,
-        };
-      }
-
-      return item;
-    });
-
-    setCart(updateCart);
-  }
-
-  function decreaseQuantity(id) {
-    const updateCart = cart.map((item) => {
-      if (item.id === id && item.quantity > 1) {
-        return {
-          ...item,
-          quantity: item.quantity - 1,
-        };
-      }
-
-      return item;
-    });
-
-    setCart(updateCart);
-  }
-
-  function clearCart() {
-    setCart([]);
-  }
-
-  function saveLocalStorage() {
-    localStorage.setItem("cart", JSON.stringify(cart));
-  }
-
-  useEffect(() => {
-    saveLocalStorage();
-  }, [cart]);
+  const {
+    data,
+    cart,
+    addToCart,
+    removeFromCart,
+    decreaseQuantity,
+    increaseQuantity,
+    clearCart,
+  } = useCart();
 
   return (
     <>
---
+++ src/hooks/useCart.js
@@ -0,0 +1,81 @@
+import { useState, useEffect } from "react";
+import { db } from "../data/db";
+
+export const useCart = () => {
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem("cart");
+    return localStorageCart ? JSON.parse(localStorageCart) : [];
+  };
+  const [data] = useState(db);
+  const [cart, setCart] = useState(initialCart);
+  const MIN_ITEMS = 1;
+
+  function addToCart(item) {
+    const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
+
+    if (itemExists < 0) {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    } else {
+      const updateCart = [...cart];
+      updateCart[itemExists].quantity++;
+      setCart(updateCart);
+    }
+  }
+
+  function removeFromCart(id) {
+    setCart((prevCart) => prevCart.filter((guitar) => guitar.id !== id));
+  }
+
+  function increaseQuantity(id) {
+    const updateCart = cart.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          quantity: item.quantity + 1,
+        };
+      }
+
+      return item;
+    });
+
+    setCart(updateCart);
+  }
+
+  function decreaseQuantity(id) {
+    const updateCart = cart.map((item) => {
+      if (item.id === id && item.quantity > MIN_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity - 1,
+        };
+      }
+
+      return item;
+    });
+
+    setCart(updateCart);
+  }
+
+  function clearCart() {
+    setCart([]);
+  }
+
+  function saveLocalStorage() {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+
+  useEffect(() => {
+    saveLocalStorage();
+  }, [cart]);
+
+  return {
+    data,
+    cart,
+    addToCart,
+    removeFromCart,
+    decreaseQuantity,
+    increaseQuantity,
+    clearCart,
+  };
+};
